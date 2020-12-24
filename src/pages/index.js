38db7fb6bef7cadb0abc2c6f6ee61ea3a2996a10
treyhay31 <treyhay@gmail.com>
2020-12-24 09:06:44 -0500

    first release

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,54 @@
 import React, { useState } from "react"
+import data from "../bible-chaps"
+import { uuid } from "uuidv4"
 
 export default function Home() {
   const [bookInput, setBookInput] = useState("")
+  const color = {
+    hovered: "#bada55",
+    regular: "inherit",
+  }
+  const changeBackground = e => {
+    e.target.style.background = color.hovered
+  }
+  const changeBackgroundBack = e => {
+    e.target.style.background = color.regular
+  }
+  const getBook = input => {
+    const results = data.fuzzySearch(data.trie, input)
 
-  const isNoMatch = input => {
-    return false
+    if (results.length > 0) {
+      return (
+        <div className="found">
+          {results.map(r => (
+            <p
+              key={uuid()}
+              onMouseEnter={changeBackground}
+              onMouseLeave={changeBackgroundBack}
+            >
+              {r} | ({data.booksAggregated[r].start}-
+              {data.booksAggregated[r].end})
+            </p>
+          ))}
+        </div>
+      )
+    }
+    return (
+      <div className="not-found">
+        <p className="typewriter">
+          Not finding '{input}' in the list of Bible books
+        </p>
+      </div>
+    )
   }
 
   const makeOutput = input => {
-    return input.length < 2 || isNoMatch(input) ? (
-      <div className="typewriter">
-        <h1>Enter the name of any book in the Bible...</h1>
-        <h1>Or you could enter the chapter number...</h1>
-        <h1>Genesis 1 being (1) and Revelation x being (1000)</h1>
+    return input.length < 2 ? (
+      <div>
+        <p className="typewriter">Enter the name of any book in the Bible...</p>
       </div>
     ) : (
-      <div>
-        <h2>here is your output</h2>
-      </div>
+      getBook(input)
     )
   }
 
@@ -34,7 +65,7 @@ export default function Home() {
           value={bookInput}
           onChange={e => setBookInput(e.target.value)}
         />
-        <label for="bible-book"> Book of the Bible </label>
+        <label htmlFor="bible-book"> Book of the Bible </label>
       </div>
       <div className="main__output">{makeOutput(bookInput)}</div>
     </div>
